4ec94ba3042d53371b9947e7881ec8ca4bb80a30
kstefanini <stefanini.k@gmail.com>
2019-05-23 15:21:18 +0200

    Trivia: Fix style

--- wordmove/provision.sh
+++ wordmove/provision.sh
@@ -3,27 +3,36 @@
 
 ## Script from https://github.com/welaika/wordmove/wiki/Getting-Wordmove-installed-in-VVV-(or-any-Vagrant)
 # Rubygems update
-if [ $(/root/.rbenv/shims/gem -v|grep '^2.') ]; then
-	echo "gem installed"
+if [ $(gem -v|grep '^2.') ]; then
+  echo "gem installed"
 else
-	apt install git-core autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev -y
-	echo "installing rbenv"
-	git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-	echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-	echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-	/root/.rbenv/bin/rbenv init -
-	git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-	echo "installing last ruby"
-	/root/.rbenv/bin/rbenv install $(/root/.rbenv/bin/rbenv install -l | grep -v - | tail -1)
-	/root/.rbenv/bin/rbenv global $(/root/.rbenv/bin/rbenv install -l | grep -v - | tail -1)
-	echo "gem: --no-document" > ~/.gemrc
+  apt-get install -y ruby-dev
+  echo "ruby-dev installed"
+  echo "gem not installed"
+  gem install rubygems-update
+  update_rubygems
 fi
 
 # wordmove install
-wordmove_install="$(/root/.rbenv/shims/gem list wordmove -i)"
+wordmove_install="$(gem list wordmove -i)"
 if [ "$wordmove_install" = true ]; then
   echo "wordmove installed"
 else
-  echo "installing wordmove"
-  /root/.rbenv/shims/gem install wordmove
+  echo "wordmove not installed"
+  gem install wordmove
+
+  wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
+  if [  "$(grep yaml $wordmove_path)" ]; then
+
+
+    echo "can require yaml"
+  else
+    echo "can't require yaml"
+    echo "set require yaml"
+
+    sed -i "7i require\ \'yaml\'" $wordmove_path
+
+    echo "can require yaml"
+
+  fi
 fi
